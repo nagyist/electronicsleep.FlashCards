b7dcbf6ba3f0b0d62771e85cb2a7124e87f8687c
Chris Robertson <crobertson@gopro.com>
2015-06-17 17:57:20 -0700

    Added create memory file generation script

---
+++ CreateMemFile.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+# Author: Chris Robertson <electronicsleep@gmail.com>
+# Date: 06/17/2015
+# Purpose: Generate a memorize.txt file for FlashCards.py
+# Released under the MIT license
+
+# Running:
+# python CreateMemFile.py Hello! 
+
+### IMPORT STANDARD FUNCTIONS ###
+
+import sys
+import traceback
+
+from random import randint
+from sys import argv
+
+if len(sys.argv)==1:
+    print 'Usage: python CreateMemFile.py Name'
+    sys.exit(0)
+
+
+script, user_name = argv
+prompt = '> '
+
+#APPEND TO MEMORIZE FILE
+
+f = open('memorize.txt', 'a')
+f.write("\n")
+f.write("#PYTHON QUESTIONS FOR MEMORIZING")
+f.write("\n")
+f.write("\n")
+
+#FOR SIMPLE TIMES TABLES
+
+for x in range(2,10):
+  for y in range(2,10):
+     times = x * y 
+     xx = str(x)
+     yy = str(y)
+     times2 = str(times)
+     f.write("Q. " + xx + " * " + yy + "\n")
+     f.write("A. " + times2 + "\n")
+     f.write("\n")
+
+#FOR DIFFICULT TIMES TABLES
+
+for x in range(11,20):
+  for y in range(1,25):
+     times = x * y 
+     f = open('memorize2.txt', 'a')
+     xx = str(x)
+     yy = str(y)
+     times2 = str(times)
+     f.write("Q. " + xx + " * " + yy + "\n")
+     f.write("A. " + times2 + "\n")
+     f.write("\n")
+ 
+#END OF SCRIPT
+sys.exit(0)
