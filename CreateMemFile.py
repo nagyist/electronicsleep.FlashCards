ba1919e6b37a8032704888069df8e5f1fbe1d648
Chris <electronicsleep@gmail.com>
2017-05-09 22:45:47 -0700

    PEP 8

--- CreateMemFile.py
+++ CreateMemFile.py
@@ -8,12 +8,11 @@
 # Running:
 # python CreateMemFile.py
 
-### IMPORT STANDARD FUNCTIONS ###
+# IMPORT STANDARD FUNCTIONS
 
 import sys
-import traceback
 
-#APPEND TO MEMORIZE FILE
+# APPEND TO MEMORIZE FILE
 
 f = open('memorize.txt', 'a')
 f.write("\n")
@@ -21,29 +20,29 @@ f.write("#MATH QUESTIONS FOR MEMORIZING")
 f.write("\n")
 f.write("\n")
 
-#FOR SIMPLE TIMES TABLES
+# FOR SIMPLE TIMES TABLES
 
-for x in range(2,10):
-  for y in range(2,10):
-     times = x * y 
-     xx = str(x)
-     yy = str(y)
-     times2 = str(times)
-     f.write("Q. " + xx + " * " + yy + "\n")
-     f.write("A. " + times2 + "\n")
-     f.write("\n")
+for x in range(2, 10):
+    for y in range(2, 10):
+        times = x * y
+        xx = str(x)
+        yy = str(y)
+        times2 = str(times)
+        f.write("Q. " + xx + " * " + yy + "\n")
+        f.write("A. " + times2 + "\n")
+        f.write("\n")
 
-#FOR DIFFICULT TIMES TABLES
+# FOR DIFFICULT TIMES TABLES
 
-for x in range(11,20):
-  for y in range(1,25):
-     times = x * y 
-     xx = str(x)
-     yy = str(y)
-     times2 = str(times)
-     f.write("Q. " + xx + " * " + yy + "\n")
-     f.write("A. " + times2 + "\n")
-     f.write("\n")
+for x in range(11, 20):
+    for y in range(1, 25):
+        times = x * y
+        xx = str(x)
+        yy = str(y)
+        times2 = str(times)
+        f.write("Q. " + xx + " * " + yy + "\n")
+        f.write("A. " + times2 + "\n")
+        f.write("\n")
  
-#END OF SCRIPT
+# END OF SCRIPT
 sys.exit(0)
