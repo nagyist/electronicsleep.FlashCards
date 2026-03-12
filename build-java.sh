a32d788b787df427d98f2a624d3c693bf326109d
Chris Robertson <electronicsleep@gmail.com>
2026-03-12 15:36:29 -0700

    move-build-java

--- build-java.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-echo "Build FlashCards Java"
-#git clone https://github.com/electronicsleep/FlashCards.git
-cd FlashCardsJava/src
-javac FlashCardsJava.java
-java FlashCardsJava
-echo "Java Build OK"
